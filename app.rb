2497b14923e60b1485fe484b2d7087d33a634fd7
Lilly Piri <lilly@lillypiri.com>
2016-03-24 12:24:30 +1000

    Edited the choices

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'erb'
 
 
 get '/' do
-  choices = ['conceptual', 'fatberg', 'enviroment', 'visual']
+  choices = ["Layers of stacked virgin pieces of fatberg from the visceral underbelly of Melbourne's sewerage system, coated in the purest Ivorcy Black pigment sourced from the charred bones of a Narwhal.", "Unnatural undulations and consistencies that draw on the surfaces of Mars, both minimalist in appearance, but evoking ancient materials that allude to the underlying narrative of humanity's concern with radical morphing of the environment", "A simultaenously intimidating and elusive physical space that inhibits the viewers ideas of understanding of the workings of alchemy within the mind's eye"']
   @choice = choices.sample
   erb :index
 end

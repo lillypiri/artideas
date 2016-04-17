1ad0fadb680a257d0d2c04135f84d0fe8fa02325
Lilly Piri <lilly@lillypiri.com>
2016-04-17 17:36:07 +1000

    new ideas added

--- app.rb
+++ app.rb
@@ -3,7 +3,15 @@ require 'erb'
 
 
 get '/' do
-  choices = ["Layers of stacked virgin pieces of fatberg from the visceral underbelly of Melbourne's sewerage system, coated in the purest Ivorcy Black pigment sourced from the charred bones of a Narwhal.", "Unnatural undulations and consistencies that draw on the surfaces of Mars, both minimalist in appearance, but evoking ancient materials that allude to the underlying narrative of humanity's concern with radical morphing of the environment", "A simultaenously intimidating and elusive physical space that inhibits the viewers ideas of understanding of the workings of alchemy within the mind's eye"]
+  choices = [
+    "Layers of stacked virgin pieces of fatberg from the visceral underbelly of Melbourne's sewerage system, coated in the purest Ivory Black pigment sourced from the charred bones of a Narwhal.",
+    "Unnatural undulations and consistencies that draw on the surfaces of Mars, both minimalist in appearance, but evoking ancient materials that allude to the underlying narrative of humanity's concern with radical morphing of the environment",
+    "A simultaenously intimidating and elusive physical space that inhibits the viewers ideas of understanding of the workings of alchemy within the mind's eye",
+    "The frayed ends of Dame Edna's best frock, being repeatedly dipped in a melted pot of silvery liquid and nutella, with rubber ducks floating amidst the chaos. This piece explores the artist's impression of socialism in the modern world.",
+    "A single match, carved by hand, from an entire giant sequoia tree, representing the tragedy of excess of the post modern lifestyle.",
+    "A shimmering sea of aluminium foil, suspended by a swathe of brocoli and pork chunks in clear gelatine; a commentary on waning European traditions within the context of Western society. The viewer is invited to explore the space and the artificial sea and to consider their own ideas of tradition.",
+    "Twelve live pigs, dressed in the finest bridal garments from Christian Dior and Chanel, being offered up on an altar to the Greek God of War, Ares. Simulatenously evoking both base human fear and inhibiting empathy."
+  ]
   @choice = choices.sample
   erb :index
 end
